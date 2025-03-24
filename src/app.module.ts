8fa20877e06ed2c839f2e7489e3a859dc749e49a
Lucas Vieira <lucasltv@gmail.com>
2025-03-24 11:32:29 -0300

    feat: add RabbitMQ and configuration support, update environment example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    RabbitMQModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.AMQP_URL!,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
