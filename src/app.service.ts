96a5d7ba070e033ede8d7242db274de1dc72e5e8
Lucas Vieira <lucasltv@gmail.com>
2025-03-24 11:28:09 -0300

    refactor: update project structure and dependencies, add initial controller and service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,8 @@
-import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  @RabbitSubscribe({
-    exchange: 'amq.fanout',
-    routingKey: 'subscribe-route',
-    queue: 'subscribe-queue',
-  })
-  public async pubSubHandler(msg: any, amqpMsg: any) {
-    // const { correlationId } = amqpMsg.properties;
-    console.log(`Received message: ${JSON.stringify(msg)} at ${new Date()}`);
+  getHello(): string {
+    return 'Hello World!';
   }
 }
